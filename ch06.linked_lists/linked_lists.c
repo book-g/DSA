6ae09b695bdb2d71bf91a81fac70a41fb276910a
gobal krishnan v <gobal.k@fieldlytics.in>
2024-06-18 18:22:19 +0530

    ll

---
+++ ch06.linked_lists/linked_lists.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void print_question(){
+    printf("\n1. Write a program to create a linked list and perform insertions and deletions of all cases. Write functions to sort and finally delete the entire list at once.");
+    printf("\n2. Write a program to create a circular linked list. Perform insertion and deletion at the beginning and end of the list.");
+    printf("\n3. Write a program to create a doubly linked list and perform insertions and deletions in all cases.");
+    printf("\n4. Write a program to create a circular doubly linked list and perform insertions and deletions at the beginning and end of the list.");
+    printf("\n5. Write a program to implement a header lisked list.");
+    printf("\n6. Write a program to store a polynomial using linked list. Also, perform addition and subtraction on two polynomials");
+}
