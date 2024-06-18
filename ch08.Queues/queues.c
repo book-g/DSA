cc0c22ad83a4b984b02e741684e4306c17e87c85
gobal krishnan v <gobal.k@fieldlytics.in>
2024-06-18 18:37:04 +0530

    queue

--- ch08.Queues/queues.c
+++ ch08.Queues/queues.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 void print_question(){
-    
+    printf("\n1. Write a program to implement a linear queue.");
+    printf("\n2. Write a program to implement a linked queue.");
+    printf("\n3. Write a program to implement a circular queue.");
+    printf("\n4. Write a program to implement input and output restricted deques.");
+    printf("\n5. Write a program to implement a priority queue.");
+    printf("\n6. Write a program to implement multiple queues.");
+    printf("\n7. Write a program which finds the solution of Josephus problem using a circular linked list.");
 }
