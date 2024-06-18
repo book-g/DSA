4405855a51f600ad8fa66aef66f90adf69578d2a
gobal krishnan v <gobal.k@fieldlytics.in>
2024-06-18 18:05:25 +0530

    question of strings

---
+++ ch04.string/string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void print_question(){
+    printf("\n1. Write a program to find the length of a string.");
+    printf("\n2. Write a program to convert the lower case characters of a string into upper case.");
+    printf("\n3. Write a program to append a string to another string.");
+    printf("\n4. Write a program to compare two string.");
+    printf("\n5. Write a program to reverse a given string.");
+    printf("\n6. Write a program to extract a substring from the middle of a given string.");
+    printf("\n7. Write a program to insert a string in the main text.");
+    printf("\n8. Write a program to delete a substring from a text.");
+    printf("\n9. Write a program to replace a pattern with another pattern in text");
+    printf("\n10. Write a program to sort the names of students.");
+    printf("\n11. Write a program to read multiple lines of text and then count the number of characters, words, and lines in the text.");
+    printf("\n12. Write a program to find whether a string in a palindrome or not.");
+    printf("\n13. Write a program to copy a string intro another string.");
+    printf("\n14. Write a program to concatenate two strings.");
+}
