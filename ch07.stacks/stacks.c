65b4c79568eb0bfbdc788747b2b78afa2ccfac3f
gobal krishnan v <gobal.k@fieldlytics.in>
2024-06-18 18:33:09 +0530

    stack

---
+++ ch07.stacks/stacks.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void print_question(){
+    printf("\n1. Write a program to perform Push, Pop, and Peek operations on a stack.");
+    printf("\n2. Write a program to implement a linked stack.");
+    printf("\n3. Write a program to implement multiple stacks.");
+    printf("\n4. Write a program to reverse a list of given numbers.");
+    printf("\n5. Write a program to check nesting of parentheses using a stack.");
+    printf("\n6. Write a program to convert an infix expression into its equivalent postfix notation.");
+    printf("\n7. Write a program to evaluate a postfix expression.");
+    printf("\n8. Write a program to convert an infix expression to a prefix expression.");
+    printf("\n9. Write a program to evaluate a prefix expression.");
+    printf("\n10. Write a program to calculate the factorial of a given number.");
+    printf("\n11. Write a program to calculate the GCD of two numbers using recursive functions.");
+    printf("\n12. Write a program to calculate exp(x,y) using recursive functions.");
+    printf("\n13. Write a program to print the Fibonacci series using recursion.");
+    
+}
