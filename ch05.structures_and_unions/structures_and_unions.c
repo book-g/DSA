9e83a721af7edccfb07eb38e3c2aecbc2700fe6f
gobal krishnan v <gobal.k@fieldlytics.in>
2024-06-18 18:12:58 +0530

    struct and union

---
+++ ch05.structures_and_unions/structures_and_unions.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void print_question(){
+    printf("\n1. Write a program using structures to read and display the information about a student.");
+    printf("\n2. Write a program to read, display, add, and subtract two complex numbers.");
+    printf("\n3. Write a program to read and display the information of a student using a nested structure.");
+    printf("\n4. Write a program to read and display the information of all the students in a class. Then edit the details of the ith student and redisplay the entire information.");
+    printf("\n5. Write a program to read, display, add, and subtract two distance. Distance must be defined using kms and meters.");
+    printf("\n6. Write a program to initialize the members of a structure by using a pointer to the structure.");
+    printf("\n7. Write a program, using an array of pointers to a structure, to read and display the data of students.");
+    printf("\n8. Write a program that passes a pointer to a structure to a function.");
+}
