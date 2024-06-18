cb2e3dc4c13af79e0aead0cb39035c9a90383f52
gobal krishnan v <gobal.k@fieldlytics.in>
2024-06-18 17:54:49 +0530

    chapter 1. question

---
+++ ch01.c_intro/introduction_to_c.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+
+void print_question(){
+printf("\n1. Write a program to calculate the area of a circle.");
+printf("\n2. Write a program to convert an integer into the corresponding floating point number.");
+printf("\n3. Write a program to find whether a number is even or ordd.");
+printf("\n4. Write a program to determine whether the entered character is a vowel or not.");
+printf("\n5. Write a program to calculate the sum of numbers from m to n.");
+printf("\n6. Write a program to calculate the average of first n numbers.");
+printf("\n7. Write a program to determine whether a given number is a prime or a composite number.");
+printf("\n8. Write a program to find whether a number is even or odd using functions.");
+printf("\n9. Write a program to add two integers using pointers and functions");
+}
